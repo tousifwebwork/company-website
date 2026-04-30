6a7151bf08158cf43b08ebac3264b4494dac1803
Your Name <your@email.com>
2026-05-01 02:55:48 +0530

    Backend URL Update

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:3000/api" || "https://company-website-u7x3.onrender.com";
+const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 // GET all openings
 export const getOpenings = () => {
